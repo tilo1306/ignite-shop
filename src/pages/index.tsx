b1db9de0bb4ba99dab550ea0469c75268303d7fa
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-26 20:19:01 -0300

    feat:  add keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,41 @@
-import { styled } from "@/styles";
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
 
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.cs";
+
 import camiseta1 from "../assets/1.png";
+import camiseta2 from "../assets/2.png";
+import camiseta3 from "../assets/3.png";
+import camiseta4 from "../assets/4.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="camiseta1" />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="camiseta1" />
+        <footer>
+          <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="camiseta1" />
+        <footer>
+          <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
